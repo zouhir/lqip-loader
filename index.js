6212b037bf52ecd0bae86923c51c865fd7be82ae
Zouhir <zouhir@zouhir.org>
2017-10-08 15:39:36 +1100

    v2.0.0-color-palette

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 // http://sharp.dimens.io/en/stable/performance/#results
 // Faster npm image processing
-var sharp = require('sharp');
 
-var loaderUtils = require('loader-utils');
-var lqipName = require('./package.json').name;
-var packageversion = require('./package.json').version;
-
+var loaderUtils = require("loader-utils");
+var lqipName = require("./package.json").name;
+var packageversion = require("./package.json").version;
+var lqip = require("lqip");
 // supported images \ mimetypes
 // best results have been seen on JPEG banners
 var SUPPORTED_MIMES = {
-  'jpeg': 'image/jpeg',
-  'jpg': 'image/jpeg',
-  'png': 'image/png'
+  jpeg: "image/jpeg",
+  jpg: "image/jpeg",
+  png: "image/png"
 };
 
 // extension: file extension
 // data: image file Buffer after resize
-var toBase64 = function (extension, data) {
-  return 'data:' + SUPPORTED_MIMES[extension] + ';base64,' + data.toString('base64');
+var toBase64 = function(extension, data) {
+  return (
+    "data:" + SUPPORTED_MIMES[extension] + ";base64," + data.toString("base64")
+  );
 };
-module.exports = function() { }
+module.exports = function() {};
 module.exports.pitch = function(content) {
   this.cacheable && this.cacheable();
   var callback = this.async();
@@ -27,7 +28,10 @@ module.exports.pitch = function(content) {
   // image file path
   var path = this.resourcePath;
   // image file extension
-  var extension = path.split('.').pop().toLowerCase();
+  var extension = path
+    .split(".")
+    .pop()
+    .toLowerCase();
 
   // the source HQ image file
   var source = null;
@@ -35,11 +39,14 @@ module.exports.pitch = function(content) {
   var presource = null;
 
   // user options
-  var baseConfig = loaderUtils.getOptions(this) || { };
+  var baseConfig = loaderUtils.getOptions(this) || {};
+
   // default options
   var config = {
-    path: '',
-    name: '[name].[ext]'
+    path: "",
+    name: "[name].[ext]",
+    base64: true,
+    palette: false
   };
   // take the user's specified options as a preference
   Object.keys(config).forEach(function(key) {
@@ -49,29 +56,56 @@ module.exports.pitch = function(content) {
   var context = config.context || this.options.context;
 
   // use loaderUtils to construct a proper name
-  // config.name eg. ([name].[ext]: car.jpg) or ([hash].[ext]: [96redfghjk.....].[jpg]) 
-  source = loaderUtils.interpolateName(this, config.path + '/' + config.name, {
-		context: context,
-		content: content
-	}) || "/";
-  
-  // output object
-  var output = {};
+  // config.name eg. ([name].[ext]: car.jpg) or ([hash].[ext]: [96redfghjk.....].[jpg])
+  source =
+    loaderUtils.interpolateName(this, config.path + "/" + config.name, {
+      context: context,
+      content: content
+    }) || "/";
 
-  if (typeof SUPPORTED_MIMES[extension] === 'undefined') {
-    throw new Error('Unsupported image format passed to ' + packageName + ' v. ' + packageversion);
+  if (typeof SUPPORTED_MIMES[extension] === "undefined") {
+    throw new Error(
+      "Unsupported image format passed to " +
+        packageName +
+        " v. " +
+        packageversion
+    );
   }
 
-  sharp(path)
-    .resize(14) // resize to 16px width and auto height
-    .toBuffer() // converts to buffer for Base64 conversion
+  var outputPromises = [];
+  // output object
+  var output = {};
+  output.src = source;
+
+  if (config.base64 === true) {
+    outputPromises.push(lqip.base64(path));
+  } else {
+    outputPromises.push(null);
+  }
+
+  if (config.palette === true) {
+    outputPromises.push(lqip.palette(path));
+  } else {
+    outputPromises.push(null);
+  }
+
+  Promise.all(outputPromises)
     .then(data => {
-      presource = toBase64(extension, data);
-      output.src = source;
-      output.preSrc = presource;
-      callback(null, 'module.exports = ' + JSON.stringify(output) + ';');
+      if (data) {
+        if (data[0]) {
+          output.preSrc = data[0];
+        }
+        if (data[1]) {
+          output.palette = data[1].palette;
+          output.dominant = data[1].dominant;
+        }
+        callback(null, "module.exports = " + JSON.stringify(output) + ";");
+      } else {
+        callback(err, null);
+      }
     })
-    .catch(err => {
+    .catch(error => {
+      console.error(error);
       callback(err, null);
     });
 };
